392c9fd7e796a46a4006d759503261930c842f51
SagHack <96527197+SagHack@users.noreply.github.com>
2022-03-01 19:39:35 +0100

    Create practica2_1.sh

---
+++ practica2_1.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+echo "Introduzca el nombre del fichero: "
+read nfich
+#Tener cuidado por que no lo hacemos para que funcione en todos los directorios
+imprimir=""
+#comprobamos que el archivo existe
+if [ -f $nfich ]
+then
+	#comprobamos el permiso de lectura
+  if [ -r $nfich ]
+  then
+    imprimir+="r"
+  else
+    imprimir="-"
+  fi
+	#comprobamos el permiso de escritura
+  if [ -w $nfich ]
+  then
+    imprimir+="w"
+  else
+    imprimir+="-"
+  fi
+	#comprobamos el permiso de ejecucion
+  if [ -x $nfich ]
+  then
+    imprimir+="x"
+  else
+    imprimir+="-"
+  fi
+  echo "Los permisos del archivo $nfich son: $imprimir"
+else 
+	echo "$nfich no existe"
+fi
+
