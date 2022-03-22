9e926751e0374e22f41728f554780b2991410972
SagHack <96527197+SagHack@users.noreply.github.com>
2022-03-22 11:18:04 +0100

    Update practica2_3.sh

--- practica2_3.sh
+++ practica2_3.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
+#818058, Arcega Vela, Hector, [T], [2], [A]
+#821038, Alonso Gutierrez, Simon, [T], [2], [A]
+
 if [ $# != 1 ]
 then
     echo "Sintaxis: practica2_3.sh <nombre_archivo>"
